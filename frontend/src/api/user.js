76e4673621606ff9e2e4ce0ee5a2d732fdf6fab8
rud15dns <rud15dns@naver.com>
2025-06-08 09:02:49 +0900

    fix: frontend axios path error

--- frontend/src/api/user.js
+++ frontend/src/api/user.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const API_URL = `${process.env.VUE_APP_API_URL}/join`;
+// const API_URL = `${process.env.VUE_APP_API_URL}/join`;
 
 export async function registerUser(userData) {
-    const response = await axios.post(`${API_URL}/register`, userData, {
+    const response = await axios.post(`${process.env.VUE_APP_API_URL}/join/register`, userData, {
         headers: { 'Content-Type': 'application/json' },
-        withCredentials: true //
+        withCredentials: true
     });
     return response.data;
 }
